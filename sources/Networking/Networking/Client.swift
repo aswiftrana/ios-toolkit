899169f547e7cc1b2557bc79bcb7385db67ba9b7
Asad Rana <asad.rana@workday.com>
2020-04-06 10:47:01 -0700

    Add debugging errors to networking layer

--- sources/Networking/Networking/Client.swift
+++ sources/Networking/Networking/Client.swift
@@ -20,7 +20,7 @@ public protocol ClientProtocol {
 public enum ClientError: Error, Equatable {
     case invalidURL
     case network(code: Int, reason: String)
-    case decoding
+    case decoding(reason: String, debugReason: String)
     case unknown
 }
 
@@ -78,8 +78,8 @@ public class Client: ClientProtocol {
                     return error
                 }
                 
-                if error is DecodingError {
-                    return .decoding
+                if let error = error as? DecodingError {
+                    return ClientError.makeDecodingError(error: error)
                 }
                 
                 return .unknown
@@ -92,8 +92,8 @@ public class Client: ClientProtocol {
 extension ClientError: LocalizedError {
     public var errorDescription: String? {
         switch self {
-        case .decoding:
-            return "Unable to decode response to specified class"
+        case .decoding(let reason):
+            return "Decoding Error \(reason)"
         case .invalidURL:
             return "Invalid URL supplied to Client"
         case .network(let code, let reason):
@@ -101,5 +101,36 @@ extension ClientError: LocalizedError {
         case .unknown:
             return "Unknow error occured while making request"
         }
+    }
+    
+    fileprivate static func makeDecodingError(error: DecodingError) -> ClientError {
+        func contextToString(_ context: DecodingError.Context) -> String {
+            let paths = context.codingPath.reduce("") { (result, key) -> String in
+                if result.isEmpty {
+                    return key.stringValue
+                } else {
+                    return result + " -> " + key.stringValue
+                }
+            }
+            
+            return "context: \(context.debugDescription), paths: \(paths)"
+        }
+        
+        
+        let debugReason: String
+        switch error {
+        case .dataCorrupted(let context):
+            debugReason = "Data is corrupted or otherwise invalid. \(contextToString(context))"
+        case .keyNotFound(let key, let context):
+            debugReason = "A keyed decoding container was asked for an entry for the given key, but did not contain one. key: \(key), \(contextToString(context))"
+        case .typeMismatch(let type, let context):
+            debugReason = "a value of the given type could not be decoded because it did not match the type of what was found in the encoded payload. type: \(type), \(contextToString(context))"
+        case .valueNotFound(let type, let context):
+            debugReason = "a non-optional value of the given type was expected, but a null value was found. type: \(type), \(contextToString(context))"
+        @unknown default:
+            debugReason = "New unhandled error"
+        }
+        
+        return .decoding(reason: error.localizedDescription, debugReason: debugReason)
     }
 }
